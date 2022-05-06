79d4d5fb10d97e6bbf3c363790546e856afa5645
ElFrigidaire <80352233+ElFrigidaire@users.noreply.github.com>
2022-05-06 18:04:10 +0200

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,45 @@
+package Model1;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Location {
+	public Date date_debut;
+	public Date date_fin;
+
+    /**
+     * Default constructor
+     */
+    public Location(Date date_debut, Date date_fin) {
+    	this.date_debut = date_debut;
+    	this.date_fin= date_fin;
+    	
+    }
+
+    /**
+     * 
+     */
+ 
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+    
+	public Scooter scoot;
+	public Retour induit;
+    /**
+     * 
+     */
+    public Client listLocation;
+
+}

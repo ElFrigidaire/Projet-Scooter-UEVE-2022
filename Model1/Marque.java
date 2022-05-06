c04df14acfb8d05308f489a6be742bb62e690e49
ElFrigidaire <80352233+ElFrigidaire@users.noreply.github.com>
2022-05-06 18:07:48 +0200

    Add files via upload

---
+++ Model1/Marque.java
@@ -0,0 +1,43 @@
+package Model1;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Marque {
+    public String nom_marque;
+    public int nombre;
+    public String nationalite;
+
+
+
+
+    /**
+     * Default constructor
+     */
+    public Marque(String nom_marque, int nombre, String nationalite) {
+    	this.nom_marque = nom_marque;
+    	this.nombre = nombre;
+    	this.nationalite = nationalite;
+    }
+
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+    public Set<Modele> listModele;
+
+}
---
+++ Model1/Retour.java
@@ -0,0 +1,34 @@
+package Model1;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Retour {
+    public Date date;
+    public String modele;
+
+    /**
+     * Default constructor
+     */
+    public Retour(Date date, String modele) {
+    	this.date = date;
+    	this.modele = modele;
+    }
+
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+
+    /**
+     * 
+     */
+    public Location signifie;
+
+}

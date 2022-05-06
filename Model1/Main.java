b07a27159317dcc6d1ddb06fc10c17357c79ad83
ElFrigidaire <80352233+ElFrigidaire@users.noreply.github.com>
2022-05-06 19:06:09 +0200

    Create Main.java

---
+++ Model1/Main.java
@@ -0,0 +1,12 @@
+package Model1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.*;
+
+public class Main {
+  	public static void main(String[] args) {
+    }
+  ArrayList<int> listescooter = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+}

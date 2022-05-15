3268772450ed082c8c1a0730f8170e8bdd083bc8
ElFrigidaire <80352233+ElFrigidaire@users.noreply.github.com>
2022-05-15 15:48:27 +0200

    Utilisations de tableaux plutôt que de listes

--- Model1/Main.java
+++ Model1/Main.java
@@ -8,8 +8,11 @@ import java.util.*;
 public class Main {
   	public static void main(String[] args) {
     }
-  ArrayList<int> listescooter = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
-  ArrayList<String> listemarques = new ArrayList<>(Arrays.asList("Yamaha", "Honda", "Piaggio", "Suzuki"));
+  //ArrayList<int> listescooter =  new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+  int[] listeScooter= new int[20];
+  
+  //ArrayList<String> listemarques = new ArrayList<>(Arrays.asList("Yamaha", "Honda", "Piaggio", "Suzuki"));
+  int[] listeMarques
   ArrayList<int> listeclients = new ArrayList<>(Arrays.asList(20200411, 20200412, 20200413, 20200414));
   
 }

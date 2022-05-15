02eaf8803a120005ee146dcd906e65e2093d9ce8
ElFrigidaire <80352233+ElFrigidaire@users.noreply.github.com>
2022-05-15 18:47:12 +0200

    Fct d'initialisation créée

--- Model1/Main.java
+++ Model1/Main.java
@@ -7,44 +7,55 @@ import java.io.ObjectOutputStream;
 import java.util.*;
 
 public class Main {
+	
+	static public Scooter[] init_BDD_scoot(int numScoot, String[] listeNomsMarques) {
+		Scooter[] listeScooter= new Scooter[numScoot];
+		for (int i=0; i<listeScooter.length;i++) {
+		  Random random = new Random();
+		  int idxMarque=random.nextInt(listeNomsMarques.length + 0);
+		  double kilometrage = random.nextDouble(1000);
+		  listeScooter[i] = new Scooter(listeNomsMarques[idxMarque], i, kilometrage);
+		  System.out.println(listeScooter[i].toString());
+		  }
+		return listeScooter;
+	}
+	
+	static public ArrayList<Client> init_BDD_clients(String[] nomsRues, String[] prenoms, String[] noms){
+		//Je choisis une arrayList car le nombre de clients va changer au fur et à mesure que les clients s'inscrivent 
+	  	ArrayList<Client> listeClients = new ArrayList<Client>();
+		for (int i = 0; i<4;i++) {
+	  		  Random random = new Random();
+	  		  int numRue = random.nextInt(90)+1;
+	  		  int idxRue = random.nextInt(nomsRues.length);
+	  		  int idxPrenom = random.nextInt(prenoms.length);
+	  		  int idxNom = random.nextInt(noms.length);
+	  		  
+	  		  String adresse = numRue+" "+nomsRues[idxRue];
+	  		  String email = prenoms[idxPrenom]+"."+noms[idxNom]+"@POO.fr";
+	  		  int age = random.nextInt(75)+18;
+	  		  
+	  		  listeClients.add(new Client(i, adresse, email, "00000000", noms[idxNom],prenoms[idxPrenom], age));
+	  		  System.out.println(listeClients.get(i).toString());
+	  		  }
+		return listeClients;
+	}
+	
   	public static void main(String[] args) {
   		//Initialisation de la BDD 
   	  String[] listeNomsMarques= {"Yamaha", "Honda", "Piaggio", "Suzuki"};
-  	  Scooter[] listeScooter= new Scooter[20];
-  	  for (int i=0; i<listeScooter.length;i++) {
-  		  Random random = new Random();
-  		  int idxMarque=random.nextInt(listeNomsMarques.length + 0);
-  		  double kilometrage = random.nextDouble(1000);
-  		  listeScooter[i] = new Scooter(listeNomsMarques[idxMarque], i, kilometrage);
-  		  System.out.println(listeScooter[i].toString());
-    }
-  	  //Je choisis une arrayList car le nombre de clients va changer au fur et à mesure que les clients s'inscrivent 
-  	  ArrayList<Client> listeClients = new ArrayList<Client>();
+  	  Scooter[] listeScooter = init_BDD_scoot(20, listeNomsMarques);
+  	  
   	  String[] nomsRues = {"avenue de POO","rue de Cacahouète","boulevard du Frigidaire"};
   	  String[] prenoms = {"Jonathan","Thomas","Plouf","Antoinette"};
   	  String[] noms = {"Barneche", "Auzannet","Tarek", "Melliti"};
+  	  ArrayList<Client> listeClients = init_BDD_clients(nomsRues,prenoms,noms);	  
   	  
-  	  
-  	  for (int i = 0; i<4;i++) {
-  		  Random random = new Random();
-  		  int numRue = random.nextInt(90)+1;
-  		  int idxRue = random.nextInt(nomsRues.length);
-  		  int idxPrenom = random.nextInt(prenoms.length);
-  		  int idxNom = random.nextInt(noms.length);
-  		  
-  		  String adresse = numRue+" "+nomsRues[idxRue];
-  		  String email = prenoms[idxPrenom]+"."+noms[idxNom]+"@POO.fr";
-  		  int age = random.nextInt(75)+18;
-  		  
-  		  listeClients.add(new Client(i, adresse, email, "00000000", noms[idxNom],prenoms[idxPrenom], age));
-  		  System.out.println(listeClients.get(i).toString());
-  }
   	 
   	  //Demande du numéro de scooter choisi
   	  
 }
-}
 
+}
   
 
 //ArrayList<String> listemarques = new ArrayList<>(Arrays.asList("Yamaha", "Honda", "Piaggio", "Suzuki"));
